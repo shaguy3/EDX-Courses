927a62ac74ee845813103cf699f62e0df5e01dbe
shaguy3 <shaguy3@gmail.com>
2018-11-29 17:05:48 +0200

    Improvedd the __repr__ method of the polynomial

--- Essential-Math-for-Machine-Learning-Python-Edition/Polynomials.py
+++ Essential-Math-for-Machine-Learning-Python-Edition/Polynomials.py
@@ -4,22 +4,28 @@ class Polynomial:
         self.coefficients = coefficients[::-1]
 
     def __repr__(self):
-        str_to_return = ""
+        representation = ""
         power_counter = self.coefficients.__len__() - 1
         for coefficient in self.coefficients[::-1]:
             if coefficient == 0:
                 pass
-            elif power_counter == 0:
-                str_to_return += str(coefficient) + " "
-            elif power_counter == 1:
-                if coefficient == 1:
-                    str_to_return += "x + "
+            elif coefficient > 0:
+                if power_counter == 0:
+                    representation += " + " + str(coefficient)
+                elif power_counter == 1:
+                    representation += " + " + str(coefficient) + "x"
                 else:
-                    str_to_return += str(coefficient) + "x" + " + "
-            elif coefficient == 1:
-                str_to_return += "x^" + str(power_counter) + " + "
+                    representation += " + " + str(coefficient) + "x^" + str(power_counter)
             else:
-                str_to_return += str(coefficient) + "x^" + str(power_counter) + " + "
+                if power_counter == 0:
+                    representation += " - " + str(abs(coefficient))
+                elif power_counter == 1:
+                    representation += " - " + str(abs(coefficient)) + "x"
+                else:
+                    representation += " - " + str(abs(coefficient)) + "x^" + str(power_counter)
             power_counter -= 1
-        return str_to_return
+        representation = representation[3:]
+        return representation
 
+
+print(Polynomial(2, -5, 0, 3).__repr__())
