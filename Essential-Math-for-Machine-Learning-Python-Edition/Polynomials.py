3f742cc5ffd8a3f1182af3b616dcd0061e0c0ce3
shaguy3 <shaguy3@gmail.com>
2018-12-01 16:26:33 +0200

    Final version for now of the polynomial class: - Fixed the __repr__ function - Fixed the __add__ function - Added __sub__ function - Added a derivative function

--- Essential-Math-for-Machine-Learning-Python-Edition/Polynomials.py
+++ Essential-Math-for-Machine-Learning-Python-Edition/Polynomials.py
@@ -13,34 +13,36 @@ class Polynomial:
                 pass
             elif coefficient == 1:
                 if power_counter == 0:
-                    representation += "+"
+                    representation += " + " + str(coefficient)
                 elif power_counter == 1:
-                    representation += "+x"
+                    representation += " + x"
                 else:
-                    representation += "+x^" + str(power_counter)
+                    representation += " + x^" + str(power_counter)
             elif coefficient == -1:
                 if power_counter == 0:
-                    representation += "-1"
+                    representation += " - " + str(abs(coefficient))
                 elif power_counter == 1:
-                    representation += "-x"
+                    representation += " - x"
                 else:
-                    representation += "-x^" + str(power_counter)
+                    representation += " - x^" + str(power_counter)
             elif coefficient > 0:
                 if power_counter == 0:
-                    representation += "+" + str(coefficient)
+                    representation += " + " + str(coefficient)
                 elif power_counter == 1:
-                    representation += "+" + str(coefficient) + "x"
+                    representation += " + " + str(coefficient) + "x"
                 else:
-                    representation += "+" + str(coefficient) + "x^" + str(power_counter)
+                    representation += " + " + str(coefficient) + "x^" + str(power_counter)
             else:
                 if power_counter == 0:
-                    representation += "-" + str(abs(coefficient))
+                    representation += " - " + str(abs(coefficient))
                 elif power_counter == 1:
-                    representation += "-" + str(abs(coefficient)) + "x"
+                    representation += " - " + str(abs(coefficient)) + "x"
                 else:
-                    representation += "-" + str(abs(coefficient)) + "x^" + str(power_counter)
+                    representation += " - " + str(abs(coefficient)) + "x^" + str(power_counter)
             power_counter -= 1
-        if self.coefficients[0] >= 0:
+        if self.coefficients[::-1][0] > 0:
+            representation = representation[3:]
+        else:
             representation = representation[1:]
         return representation
 
@@ -53,6 +55,8 @@ class Polynomial:
 
     @staticmethod
     def zip_longest(iter1, iter2, fillchar=None):
+        """A static method that returns a tuple with two coefficients, one from each polynomial, and fills empty spots
+        with zeros, so the usr can perform actions on different sizes of polynomials"""
         for i in range(max(len(iter1), len(iter2))):
             if i >= len(iter1):
                 yield (fillchar, iter2[i])
@@ -60,16 +64,26 @@ class Polynomial:
                 yield (iter1[i], fillchar)
             else:
                 yield (iter1[i], iter2[i])
-            i += 1
 
     def __add__(self, other):
-        c1 = self.coefficients[::-1]
-        c2 = other.coefficients[::-1]
-        res = [sum(t) for t in Polynomial.zip_longest(c1, c2)]
-        return Polynomial(*res)
+        """Returns a Polynomial that is the sum of two other polynomials"""
+        c1 = self.coefficients
+        c2 = other.coefficients
+        res = [sum(t) for t in Polynomial.zip_longest(c1, c2, 0)]
+        return Polynomial(*res[::-1])
 
+    def __sub__(self, other):
+        """Returns a polynomial that is the difference between two other polynomials"""
+        c1 = self.coefficients
+        c2 = other.coefficients
+        res = [t1-t2 for t1, t2 in Polynomial.zip_longest(c1, c2, 0)]
+        return Polynomial(*res[::-1])
 
-p1 = Polynomial(-1, 2, -1)
-print(p1)
-p2 = Polynomial(-6, 4)
-print(p2)
+    def derivative(self):
+        """Returns the derivative of a certain polynomial"""
+        res = []
+        for index, coeff in enumerate(self.coefficients):
+            if not index == 0:
+                res.append(index * coeff)
+        return Polynomial(*res[::-1])
+
